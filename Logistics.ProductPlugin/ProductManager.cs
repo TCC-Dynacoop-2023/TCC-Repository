aa36c3d6a7a7ec5e02afc71ed15487b92b7227f0
tiago vazzoller <tiago_rv0@hotmail.com>
2023-02-26 18:08:17 -0300

    feat: main project

---
+++ Logistics.ProductPlugin/ProductManager.cs
@@ -0,0 +1,13 @@
+﻿using Logistics.ProductPlugin.LogisticsCore;
+using System;
+
+namespace Logistics.ProductPlugin
+{
+    public class ProductManager : PluginCore
+    {
+        public override void ExecutePlugin(IServiceProvider serviceProvider)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
